2765307bbf88fda0b0f3769f43e4b03a7361049e
Duyvd04 <vuducduy1112004@gmail.com>
2024-11-04 00:15:55 +0700

    demo video

--- FaceBook/app/src/main/java/vn/edu/usth/facebook/Video/VideoAdapter.java
+++ FaceBook/app/src/main/java/vn/edu/usth/facebook/Video/VideoAdapter.java
@@ -2,43 +2,60 @@ package vn.edu.usth.facebook.Video;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.List;
-
 import vn.edu.usth.facebook.R;
 
-public class VideoAdapter extends RecyclerView.Adapter<VideoViewHolder> {
+public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHolder> {
 
-    Context context;
-    List<VideoItem> items;
+    private final List<String> videoUrls;
+    private final Context context;
 
-    public VideoAdapter(Context context, List<VideoItem> items) {
+    public VideoAdapter(Context context, List<String> videoUrls) {
         this.context = context;
-        this.items = items;
+        this.videoUrls = videoUrls;
     }
 
     @NonNull
     @Override
     public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new VideoViewHolder(LayoutInflater.from(context).inflate(R.layout.post_frame,parent, false));
+        View view = LayoutInflater.from(context).inflate(R.layout.video_item, parent, false);
+        return new VideoViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull VideoViewHolder holder, int position) {
-        holder.nameView.setText(items.get(position).getName());
-        holder.timeView.setText(items.get(position).getTime());
-        holder.contentView.setText(items.get(position).getContent());
-        holder.avatarView.setImageResource(items.get(position).getAvatar());
-        holder.postView.setImageResource(items.get(position).getPostimage());
-
+        String videoUrl = videoUrls.get(position);
+        holder.bind(videoUrl);
     }
 
     @Override
     public int getItemCount() {
-        return items.size();
+        return videoUrls.size();
+    }
+
+    public static class VideoViewHolder extends RecyclerView.ViewHolder {
+        private final WebView videoWebView;
+
+        public VideoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            videoWebView = itemView.findViewById(R.id.videoWebView);
+        }
+
+        public void bind(String videoUrl) {
+            videoWebView.setWebViewClient(new WebViewClient());
+            WebSettings webSettings = videoWebView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+
+            String html = "<html><body style=\"margin:0;padding:0;\"><iframe width=\"100%\" height=\"100%\" src=\""
+                    + videoUrl + "\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
+            videoWebView.loadData(html, "text/html", "utf-8");
+        }
     }
 }
--- FaceBook/app/src/main/java/vn/edu/usth/facebook/Video/VideoFragment.java
+++ FaceBook/app/src/main/java/vn/edu/usth/facebook/Video/VideoFragment.java
@@ -1,79 +1,50 @@
 package vn.edu.usth.facebook.Video;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.Collections;
 import vn.edu.usth.facebook.R;
 
 public class VideoFragment extends Fragment {
 
+    private RecyclerView recyclerView1, recyclerView2, recyclerView3;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_video, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_video, container, false);
 
-        RecyclerView recyclerview1 = v.findViewById(R.id.recyclerviewvideo1);
+        // Initialize the RecyclerViews
+        recyclerView1 = view.findViewById(R.id.recyclerviewvideo1);
+        recyclerView2 = view.findViewById(R.id.recyclerviewvideo2);
+        recyclerView3 = view.findViewById(R.id.recyclerviewvideo3);
 
-        List<VideoItem> item1s = new ArrayList<VideoItem>();
-        item1s.add(new VideoItem("Review Phim", "7h", "Review phim hot nhất 2019: Nghề siêu khó", R.drawable.vid_ava_1, R.drawable.vid_1));
-        item1s.add(new VideoItem("Review Netflix", "8h","Bộ phim gây chấn động 2018: Ảo ảnh", R.drawable.vid_ava_2, R.drawable.vid_2));
+        // Set LayoutManager for each RecyclerView
+        recyclerView1.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView2.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView3.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        recyclerview1.setLayoutManager(new LinearLayoutManager(requireContext()));
-        recyclerview1.setAdapter(new VideoAdapter(requireContext(), item1s));
+        // Define video URLs for each RecyclerView
+        String videoUrl1 = "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/video.php?v=1531448284152067";
+        String videoUrl2 = "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/video.php?v=1531448284152067";
+        String videoUrl3 = "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/video.php?v=1531448284152067";
 
+        // Set up each RecyclerView with its own video URL
+        VideoAdapter adapter1 = new VideoAdapter(getContext(), Collections.singletonList(videoUrl1));
+        VideoAdapter adapter2 = new VideoAdapter(getContext(), Collections.singletonList(videoUrl2));
+        VideoAdapter adapter3 = new VideoAdapter(getContext(), Collections.singletonList(videoUrl3));
 
+        recyclerView1.setAdapter(adapter1);
+        recyclerView2.setAdapter(adapter2);
+        recyclerView3.setAdapter(adapter3);
 
-        RecyclerView recyclerview2 = v.findViewById(R.id.recyclerviewvideo2);
-
-        List<VideoItem> item2s = new ArrayList<VideoItem>();
-        item2s.add(new VideoItem("Javalorant", "15h", "Valorant hightlight #6", R.drawable.vid_ava_3, R.drawable.vid_3));
-        item2s.add(new VideoItem("CSSGO", "19h", "Quan Trọng Là Tốc Độ Nhưng Là CS2", R.drawable.vid_ava_4, R.drawable.vid_4));
-
-        recyclerview2.setLayoutManager(new LinearLayoutManager(requireContext()));
-        recyclerview2.setAdapter(new VideoAdapter(requireContext(), item2s));
-
-
-
-
-        RecyclerView recyclerview3 = v.findViewById(R.id.recyclerviewvideo3);
-
-        List<VideoItem> item3s = new ArrayList<VideoItem>();
-        item3s.add(new VideoItem("500Bros CS2", "1h", "",R.drawable.vid_ava_4, R.drawable.vid_5));
-
-        recyclerview3.setLayoutManager(new LinearLayoutManager(requireContext()));
-        recyclerview3.setAdapter(new VideoAdapter(requireContext(), item3s));
-
-
-        ImageButton searchbutton = v.findViewById(R.id.search_video_button);
-        searchbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(requireContext(), vn.edu.usth.facebook.More.Video_Search_Activity.class );
-                startActivity(i);
-            }
-        });
-
-        ImageButton savebutton = v.findViewById(R.id.save_video_button);
-        savebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(requireContext(), vn.edu.usth.facebook.More.Video_Save_Activity.class );
-                startActivity(i);
-            }
-        });
-
-        return v;
+        return view;
     }
 }
